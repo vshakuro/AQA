3c50aa6d7e9407d9026e92263fc4bae1bc122639
vshakuro <vladislavshakuro@yandex.ru>
2023-08-28 16:43:51 +0300

    adding methods

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello world!");
+        printThreeWords();
+        res();
+        printColor();
+        compareNumbers();
+        }
+
+        public static void printThreeWords(){
+            System.out.println("Orange\nBanana\nApple");
+        }
+
+        public static void res(){
+            int a = 5;
+            int b = 10;
+            int sum = a + b;
+            if (sum >= 0){
+                System.out.println("Сумма положительная");
+            }
+            else{
+                System.out.println("Сумма отрицательная");
+            }
+    }
+    public static void printColor(){
+        int value = 101;
+            if (value <= 0)
+        {System.out.println("Красный");}
+            else if (value > 0 && value <= 100){
+                System.out.println("Желтый");
+            } else {
+                System.out.println("Зеленый");}
+        }
+        public static void compareNumbers(){
+        int a = 5;
+        int b = 10;
+        if (a >= b){
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
         }
     }
