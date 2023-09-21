14268cb3787469ac5a79781f367f9cf588c3d46b
vshakuro <vladislavshakuro@yandex.ru>
2023-09-21 15:11:43 +0300

    Adding answers

---
+++ src/main/java/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException() {
+        super();
+    }
+}
---
+++ src/main/java/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException() {
+        super("Некорректный размер массива");
+    }
+}
