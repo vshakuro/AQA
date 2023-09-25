d1c3b819d32246118b888a3c9c23c169e08493b3
vshakuro <vladislavshakuro@yandex.ru>
2023-09-25 14:57:54 +0300

    Adding answers

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
 package main.java;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        String[] cars = {"BMW","Mercedes","Mercedes","Volkswagen",
+                "Audi","Audi","GMC","GMC",
+                "Ford","Dodge","Ferrari","Lamborghini",
+                "Renault","Pontiac","Nissan","Toyota",};
+        Set<String> uniqueCars = new LinkedHashSet<>(Arrays.asList(cars));
+        Map<String, Integer> carsFrequency = new HashMap<>();
+        for (String uniqueCar : uniqueCars){
+            carsFrequency.put(uniqueCar, Collections.frequency(List.of(cars), uniqueCar));
+        }
+        System.out.println("Уникальные машины: " + uniqueCars);
+        System.out.println(carsFrequency);
     }
 }
