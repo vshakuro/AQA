30e7d743a8ff19e3ac6b19b38aada5bb4bffbf53
vshakuro <vladislavshakuro@yandex.ru>
2023-10-22 23:39:38 +0300

    Adding answers

--- src/main/java/myOrg/com/App.java
+++ src/main/java/myOrg/com/App.java
@@ -1,19 +1,6 @@
 package myOrg.com;
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println(factorial(-5));
-    }
-    public static int factorial(int number) {
-        if (number < 0) {
-            throw new IllegalArgumentException("Число не может быть меньше нуля");
-        }
 
-        int res = 1;
-        for (int i = 2; i <= number; i++) {
-            res = res * i;
-        }
-        return res;
+public class App {
+    public static void main(String[] args) {
     }
 }
--- src/test/java/myOrg/com/AppTest.java
+++ src/test/java/myOrg/com/AppTest.java
@@ -1,26 +1,67 @@
 package myOrg.com;
 
-import static myOrg.com.App.factorial;
-import org.testng.Assert;
-import org.testng.annotations.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.*;
+import java.util.stream.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
 public class AppTest {
-    @Test (dataProvider = "data-correct")
-    public void correctNumbers(int number, int factorial) {
-        Assert.assertEquals(factorial, factorial(number));
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().window().maximize();
+        driver.get("https://www.mts.by/");
     }
-    @Test (dataProvider = "data-incorrect", expectedExceptions = IllegalArgumentException.class)
-    public void incorrectNumbers(int number){
-        factorial(number);
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
     }
-    @DataProvider (name = "data-incorrect")
-    public Object[][] incorrectData()
-    {
-        return new Object[][] {{-5}};
+    @Test
+    void checkBlockNameTest() {
+        List<WebElement> elements = driver.findElements(By.xpath("//section/div/h2"));
+        String actual = elements.stream().map(WebElement::getText).collect(Collectors.joining());
+        String expected = "Онлайн пополнение\n".concat("без комиссии");
+        Assertions.assertEquals(expected, actual);
     }
-    @DataProvider (name = "data-correct")
-    public Object[][] correctData()
-    {
-        return new Object[][] {{5, 120}, {1, 1}, {0, 1}};
+
+    @Test
+    void findLogoPaySysTest() {
+        List<WebElement> icons = driver.findElements(By.xpath("//div[@class='pay__partners']/ul/li/img"));
+        assertAll(() -> assertTrue(icons.get(0).isDisplayed()),
+                () -> assertTrue(icons.get(1).isDisplayed()),
+                () -> assertTrue(icons.get(2).isDisplayed()),
+                () -> assertTrue(icons.get(3).isDisplayed()),
+                () -> assertTrue(icons.get(4).isDisplayed()),
+                () -> assertTrue(icons.get(5).isDisplayed()));
+    }
+
+    @Test
+    public void testMoreInfoLink() {
+        WebElement link = driver.findElement(By.xpath("//div[@class='pay__wrapper']/a"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(link).click().perform();
+        String url = driver.getCurrentUrl();
+        Assertions.assertEquals("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/", url);
+    }
+
+    @Test
+    public void testContinueButton() {
+        driver.findElement(By.xpath("//button[@class='select__header']")).click();
+        driver.findElement(By.xpath("//p[@class='select__option'][1]")).click();
+        WebElement phoneField = driver.findElement(By.id("connection-phone"));
+        phoneField.click();
+        phoneField.sendKeys("297777777");
+        WebElement continueButton = driver.findElement(By.xpath("//form[@id='pay-connection']/button[@type='submit'][1]"));
+        continueButton.click();
     }
 }
